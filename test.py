46ae5724098121f2510961021a2c1eb3daa78bc5
Adam Starbuck <me.starbuck@gmail.com>
2019-04-09 14:49:28 -0500

    Update test.py for switching tests

--- test.py
+++ test.py
@@ -12,15 +12,18 @@ tick = 0
 while True:
 
 	response = requests.request("GET", url, headers=headers)
-	print(tick)
+	# print(tick)
 	tick=tick+1
 	# print(json.loads(response.text)[0]["extruders"][0]["hotend"]["offset"]["state"])
 	print("::",json.loads(response.text)["x"],json.loads(response.text)["y"])
 
-	time.sleep(1)
+	time.sleep(0.25)
 
 
 # Ultimaker S5 Test Results
 #330.0 237.0 <--- Going with this one first
 #330.0 219.0
 
+#Ultimaker 3 printcore switching tests
+# X:211.26 Y:174.24 #printcore 2 down
+# X:209.77 Y:193.18 #printcore 2 up
