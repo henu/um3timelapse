37b19028f22b60ccfec087851828519097c2397f
Henrik Heino <henrik.heino@gmail.com>
2025-04-29 22:04:43 +0300

    Make it possible to keep program running and recording many videos

--- timelapse.py
+++ timelapse.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import os
 import argparse
+import datetime
 from requests import exceptions
-from tempfile import mkdtemp
+import tempfile
 from urllib.request import urlopen
 from um3api import Ultimaker3
 import time
@@ -10,20 +11,22 @@ import re
 import collections
 from PIL import Image
 
-cliParser = argparse.ArgumentParser(
-    description='Creates a time lapse video from the onboard camera on your Ultimaker 3.')
-cliParser.add_argument(
-    'HOST', type=str, help='IP address of the Ultimaker 3')
-cliParser.add_argument(
-    'OUTFILE', type=str, help='Name of the video file to create. Recommended formats are .mkv or .mp4.')
-options = cliParser.parse_args()
 
-imgurl = f'http://{options.HOST}:8080/?action=snapshot'
-
-api = Ultimaker3(options.HOST, 'Timelapse')
+# TODO: Remind user that firewall is needed to be turned off. Developer mode is not needed
 
 
-def get_status():
+def parse_args():
+
+    args = argparse.ArgumentParser(
+        description='Monitors your Ultimaker 3/S3 printer, and creates timelapse videos of all prints.'
+    )
+    args.add_argument('host', type=str, help='Hotsname or IP address of your printer')
+    args.add_argument('directory', type=str, help='Directory where videos are stored.')
+
+    return args.parse_args()
+
+
+def get_status(api):
     # If the printer gets disconnected, retry indefinitely
     while True:
         try:
@@ -37,78 +40,87 @@ def get_status():
                     continue
                 # TODO: Is there any state I missed?
                 if state not in ['pre_print', 'printing', 'post_print', 'wait_cleanup', 'wait_user_action', 'message']:
-                    raise Exception(f'Unknown printing state \"{state}\"!')
+                    raise Exception(f'Unknown printing state "{state}"!')
                 return state
             else:
-                raise Exception(f'Unknown printer status \"{status}\"!')
+                raise Exception(f'Unknown printer status "{status}"!')
         except exceptions.ConnectionError as err:
-            print_error(err)
+            print(f'Connection error: {err}')
+            print('Retrying...')
             time.sleep(1)
 
 
-def print_error(err):
-    print('Connection error: {0}'.format(err))
-    print('Retrying')
-    print()
+if __name__ == '__main__':
 
+    options = parse_args()
 
-tmpdir = mkdtemp()
-filenameformat = os.path.join(tmpdir, '%05d.jpg')
-print(':: Saving images to', tmpdir)
+    api = Ultimaker3(options.host, 'Timelapse')
 
-if not os.path.exists(tmpdir):
-    os.makedirs(tmpdir)
+    imgurl = f'http://{options.host}:8080/?action=snapshot'
 
-print(':: Waiting for print to start')
-while get_status() != 'printing':
-    time.sleep(1)
-print(':: Printing')
+    while True:
 
-while get_status() == 'printing':
-    # Take screenshot
-    response = urlopen(imgurl)
+        # Wait until printing starts
+        print('Waiting for new print to start')
+        while get_status(api) != 'printing':
+            time.sleep(1)
+        print('Printing started!')
 
-    # Use current time and print head to form a filename that contains all the necessary data
-    printhead_pos = api.get('api/v1/printer/heads/0/position').json()
-    filename = '{}_{}_{}_{}.jpg'.format(
-        time.time(),
-        printhead_pos['x'],
-        printhead_pos['y'],
-        printhead_pos['z'],
-    )
+        print_started = datetime.datetime.now(datetime.timezone.utc)
 
-    # Write screenshot to temporary directory
-    with open(os.path.join(tmpdir, filename), 'wb') as f:
-        f.write(response.read())
+        # Create a directory for storing frames
+        with tempfile.TemporaryDirectory() as tmpdir:
 
-    time.sleep(0.5)
+            filenameformat = os.path.join(tmpdir, '%05d.jpg')
 
-# Iterate all captured frames, and group them by their layer
-print(':: Blending captured images into frames')
-# First collect all images from same layer
-IMAGE_FILENAME_RE = re.compile('^(?P<timestamp>[\\d\\.]+)_(?P<x>[\\d\\.]+)_(?P<y>[\\d\\.]+)_(?P<z>[\\d\\.]+).jpg$')
-images_by_layers = collections.defaultdict(list)
-for image_path in os.listdir(tmpdir):
-    image_match = IMAGE_FILENAME_RE.match(image_path)
-    if image_match:
-        layer_key = str(round(100 * float(image_match.groupdict()['z']))).zfill(10)
-        images_by_layers[layer_key].append(image_path)
+            print('Saving images to', tmpdir)
 
-# Convert multiple images in layers into single frames
-frame_num = 0
-for layer_key, layer_images in sorted(images_by_layers.items()):
-    # Open all images with PIL and blend them into one
-    final_image = Image.open(os.path.join(tmpdir, layer_images[0]))
-    for layer_image_i in range(1, len(layer_images)):
-        with Image.open(os.path.join(tmpdir, layer_images[layer_image_i])) as image:
-            final_image = Image.blend(final_image, image, 1 / (layer_image_i + 1))
-    # Write frame to disk
-    final_image.save(filenameformat % frame_num)
-    frame_num += 1
+            # TODO: Check what happens when material runs out. Could we pause frame capturing during that time?
+            while get_status(api) == 'printing':
+                # Take screenshot
+                response = urlopen(imgurl)
 
-print()
-print(':: Encoding video')
-ffmpegcmd = f'ffmpeg -r 30 -i {filenameformat} -vcodec libx264 -preset veryslow -crf 18 -loglevel panic {options.OUTFILE}'
-print(ffmpegcmd)
-os.system(ffmpegcmd)
-print(':: Done!')
+                # Use current time and print head to form a filename that contains all the necessary data
+                printhead_pos = api.get('api/v1/printer/heads/0/position').json()
+                filename = '{}_{}_{}_{}.jpg'.format(
+                    time.time(),
+                    printhead_pos['x'],
+                    printhead_pos['y'],
+                    printhead_pos['z'],
+                )
+
+                # Write screenshot to temporary directory
+                with open(os.path.join(tmpdir, filename), 'wb') as f:
+                    f.write(response.read())
+
+                time.sleep(0.5)
+
+            # TODO: Do the video encoding in seprate thread, so we don't miss the next print!
+
+            # Printing is now complete.  Iterate all captured frames, and group them by their layer
+            print('Blending captured images into frames')
+            # First collect all images from same layer
+            IMAGE_FILENAME_RE = re.compile('^(?P<timestamp>[\\d\\.]+)_(?P<x>[\\d\\.]+)_(?P<y>[\\d\\.]+)_(?P<z>[\\d\\.]+).jpg$')
+            images_by_layers = collections.defaultdict(list)
+            for image_path in os.listdir(tmpdir):
+                image_match = IMAGE_FILENAME_RE.match(image_path)
+                if image_match:
+                    layer_key = str(round(100 * float(image_match.groupdict()['z']))).zfill(10)
+                    images_by_layers[layer_key].append(image_path)
+            # Convert multiple images in layers into single frames
+            frame_num = 0
+            for layer_key, layer_images in sorted(images_by_layers.items()):
+                # Open all images with PIL and blend them into one
+                final_image = Image.open(os.path.join(tmpdir, layer_images[0]))
+                for layer_image_i in range(1, len(layer_images)):
+                    with Image.open(os.path.join(tmpdir, layer_images[layer_image_i])) as image:
+                        final_image = Image.blend(final_image, image, 1 / (layer_image_i + 1))
+                # Write frame to disk
+                final_image.save(filenameformat % frame_num)
+                frame_num += 1
+
+            print('Encoding video...')
+            video_path = os.path.join(options.directory, 'print_{}.mp4'.format(print_started.isoformat()))
+            ffmpegcmd = f'ffmpeg -r 30 -i {filenameformat} -vcodec libx264 -preset veryslow -crf 18 -loglevel panic "{video_path}"'
+            os.system(ffmpegcmd)
+            print('Done!')
